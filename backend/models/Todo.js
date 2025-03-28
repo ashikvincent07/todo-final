4c138c1a453479e1888ea6521068c12f579e3ca6
ashikvincent07 <126868920+ashikvincent07@users.noreply.github.com>
2025-03-28 12:46:55 +0530

    Delete backend directory

--- backend/models/Todo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const todoSchema = new mongoose.Schema({
-  description: { type: String, required: true },
-  status: { type: String, enum: ['ongoing', 'completed'], default: 'ongoing' },
-});
-
-module.exports = mongoose.model('Todo', todoSchema);
